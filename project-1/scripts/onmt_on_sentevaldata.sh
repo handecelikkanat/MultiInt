0ae8c6475e1c3c079e628988ac665323fdec67a6
Hande Celikkanat <hande.celikkanat@helsinki.fi>
2019-06-17 12:43:59 +0300

    onmt shards bug

--- project-1/scripts/onmt_on_sentevaldata.sh
+++ project-1/scripts/onmt_on_sentevaldata.sh
@@ -11,7 +11,7 @@ SENTEVAL_TASKS=("bigram_shift")
 for TASK in ${SENTEVAL_TASKS[*]}; do
     BPE_FILE=$BASE/data/sample_bpe/$TASK.txt.bpe
     OUTPUT_FILE=$BASE/outputs/$TASK.tran
-    REPRS_FILE=$BASE/outputs/$TASK.reps.pickle
+    REPRS_FILE=$BASE/outputs/$TASK.reprs.pickle
     
     cut -f4 $BPE_FILE > bpe_temp
     python3 $ONMT/translate.py -model $MODEL_FILE -src bpe_temp -output $OUTPUT_FILE -representations_file $REPRS_FILE -replace_unk -verbose -gpu 0
