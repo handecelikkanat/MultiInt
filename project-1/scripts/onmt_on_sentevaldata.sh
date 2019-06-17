4e0be3d45ed48a0f4364c1f3911d1391228fddeb
Hande Celikkanat <hande.celikkanat@helsinki.fi>
2019-06-17 12:20:10 +0300

    gpu added

--- project-1/scripts/onmt_on_sentevaldata.sh
+++ project-1/scripts/onmt_on_sentevaldata.sh
@@ -1,4 +1,4 @@
-BASE=/Users/celikkan/Github/MultiInt/project-1/
+BASE=/home/celikkan/Github/MultiInt/project-1/
 ONMT=$BASE/opennmtsrc
 
 MODEL_FILE=$BASE/models/transformer.model_step_200000.pt
@@ -14,7 +14,7 @@ for TASK in ${SENTEVAL_TASKS[*]}; do
     REPRS_FILE=$BASE/outputs/$TASK.reps.pickle
     
     cut -f4 $BPE_FILE > bpe_temp
-    python3 $ONMT/translate.py -model $MODEL_FILE -src bpe_temp -output $OUTPUT_FILE -representations_file $REPRS_FILE -replace_unk -verbose
+    python3 $ONMT/translate.py -model $MODEL_FILE -src bpe_temp -output $OUTPUT_FILE -representations_file $REPRS_FILE -replace_unk -verbose -gpu 0
     
 #parse the correct sentence from BPE_FILE=$BASE/data/sample_bpe/$TASK.txt.bpe and change with keys.
     # it will be a list of dictionaries.
